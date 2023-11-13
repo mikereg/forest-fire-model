cba0117b59791cc849da094604365da3dc91abb6
Michael Reginiano <mike.reginiano@gmail.com>
2023-11-13 22:26:10 +0100

    initial demo with visualisation

--- forest-fire.py
+++ forest-fire.py
@@ -6,12 +6,18 @@ Created on Thu Nov  9 22:51:15 2023
 @author: michaelreginiano
 """
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import draw, pause
+
+
+l = 100 #length of grid
 
 #generating initial random forest
-grid = np.random.randint(0,2,(5,5))
-f = 0.1
-p = 0.001
+grid = np.random.randint(0,2,(l,l))
 
+
+f = 0.001
+p = 0.005
 
 def moore_neighbourhood(x,y,grid):
     x_lower,x_upper = 1,1
@@ -40,15 +46,22 @@ def moore_neighbourhood(x,y,grid):
     
     return grid[row_start:row_end,col_start:col_end]
 
-for _ in range(0,100):    
+
+fig = plt.figure()
+ax = fig.gca()
+board_image = ax.imshow(grid,cmap='Greens',vmin=0,vmax=2)
+
+
+for _ in range(0,100):
+    board_image.set_data(grid)
+    board_image.norm.autoscale([0,1,2])
+    draw()
+    pause(0.5)
     grid[grid == -1] = 0
-        
     x,y = np.where(grid == 1)[0], np.where(grid == 1)[1]
     trees = np.array([(x[i],y[i]) for i in range(0,len(x))], dtype=tuple)
     for t in trees:
         if any((moore_neighbourhood(t[0], t[1], grid) == -1).flatten()):
             grid[t[0],t[1]] == -1
-            
-    grid[grid == 1] = np.random.choice([1,-1],1,p=[1-f,f])
-    grid[grid == 0] = np.random.choice([0,1],1,p=[1-p,p])
-    print(grid)
+    grid[grid == 1] = np.random.choice([1,-1],len(trees),p=[1-f,f])
+    grid[grid == 0] = np.random.choice([0,1],(l**2)-len(trees),p=[1-p,p])
